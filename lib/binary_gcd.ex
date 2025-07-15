3528e7dbb4d356e9467eb9b655f85b453d3b4f55
Susumu Yamazaki <zacky1972@gmail.com>
2025-07-16 06:14:58 +0900

    initial commit

---
+++ lib/binary_gcd.ex
@@ -0,0 +1,18 @@
+defmodule BinaryGcd do
+  @moduledoc """
+  Documentation for `BinaryGcd`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BinaryGcd.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/binary_gcd_test.exs
@@ -0,0 +1,8 @@
+defmodule BinaryGcdTest do
+  use ExUnit.Case
+  doctest BinaryGcd
+
+  test "greets the world" do
+    assert BinaryGcd.hello() == :world
+  end
+end
